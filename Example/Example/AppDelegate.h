6c7d5f6ff8aa348c3d078d510a65f438d6c39b28
X140Yu <zhaoxinyu1994@gmail.com>
2018-03-29 14:12:01 +0800

    Recreate Example project

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by X140Yu on 29/3/2018.
+//  Copyright © 2018 X140Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by X140Yu on 29/3/2018.
+//  Copyright © 2018 X140Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
